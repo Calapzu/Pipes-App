5cb270f72cc0fccfb965f5e0bf056b8626850455
Calapzu <yamsoncalapzu@gmail.com>
2023-08-19 15:43:51 -0500

    I18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n select
+  public name: string = 'Yamson';
+  public gender: 'male'|'female' = 'male';
+
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Melissa';
+    this.gender ='female';
+  }
 }
