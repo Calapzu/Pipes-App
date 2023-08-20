c0f34e0eef5f37451c96993b2adcb9bbe6cb2483
Calapzu <yamsoncalapzu@gmail.com>
2023-08-19 21:50:56 -0500

    PrimeNG - Table

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -7,8 +7,8 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
-
 import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 
 @NgModule({
@@ -19,7 +19,8 @@ import { ToolbarModule } from 'primeng/toolbar';
     FieldsetModule,
     //MenuModule,
     PanelModule,
-    ToolbarModule
+    ToolbarModule,
+    TableModule
   ]
 })
 export class PrimeNgModule { }
